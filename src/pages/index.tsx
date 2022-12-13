bb8071c92e1980f0a70d5ce30d80c00e5767fc81
Anton <a.chertok@akatech.com.ua>
2022-12-13 11:57:08 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,25 +3,34 @@ import Heading from "../components/Heading";
 import styles from "../styles/Home.module.scss";
 import { FC } from "react";
 import { socialsType } from "../types";
-import { server } from "../Config";
+// import { server } from "../Config";
 
 export const getStaticProps = async () => {
   // const response = await fetch(`${process.env.API_HOST}/socials`);
   // const response = await fetch(`http://localhost:3000/api/socials`);
-  const response = await fetch(`${server}/api/socials`);
-  const socials = await response.json();
+  //  const response = await fetch(`${server}/api/socials`);
+  try {
+    const response = await fetch(`${process.env.API_HOST}/socials`);
+    const socials = await response.json();
 
-  if (!socials) {
+    if (!socials) {
+      return {
+        notFound: true,
+      };
+    }
+
     return {
-      notFound: true,
+      props: {
+        socials,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        socials: null,
+      },
     };
   }
-
-  return {
-    props: {
-      socials,
-    },
-  };
 };
 
 type homePropsType = {
@@ -29,22 +38,25 @@ type homePropsType = {
 };
 
 const Home: FC<homePropsType> = ({ socials }) => {
-  console.log(socials);
+  // console.log(socials);
+
   return (
     <div className={styles.wrapper}>
       <Head>
         <title>Home</title>
       </Head>
       <Heading text="Home" />
-      <ul>
-        {socials.map(({ id, icon, path }) => (
-          <li key={id}>
-            <a href={path} rel="noopener norefferer">
-              <span>{icon}</span>
-            </a>
-          </li>
-        ))}
-      </ul>
+      {socials && (
+        <ul>
+          {socials.map(({ id, icon, path }) => (
+            <li key={id}>
+              <a href={path} rel="noopener norefferer">
+                <span>{icon}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
